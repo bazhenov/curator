e71faff02293140b4255aa1900f80bf3a0a5a219
Denis Bazhenov <bazhenov@farpost.com>
2020-03-31 18:16:31 +1000

    Buffering iterator over lines added, error-chain added

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+#[macro_use]
+extern crate error_chain;
+
 pub mod client;
 pub mod server;
 mod sse;
+
+mod errors {
+
+    error_chain! {
+      foreign_links {
+        Fmt(::std::fmt::Error);
+        Utf(::std::string::FromUtf8Error);
+        Io(::std::io::Error);
+        Http(::actix_web::http::Error);
+        Hyper(hyper::error::Error);
+      }
+
+      errors {
+        UnknownError
+      }
+    }
+}
--- tests/curator_sse_test.rs
+++ tests/curator_sse_test.rs
@@ -1,20 +1,32 @@
 extern crate curator;
 
-use curator::client::CuratorClient;
+use curator::client::SseClient;
 use curator::server::Curator;
+use std::error::Error;
 use std::thread;
+use std::time::Duration;
 use tokio;
 use tokio::time::delay_for;
-use std::time::Duration;
 
 #[actix_rt::test]
-async fn curator_sse_client() {
-    let server = Curator::start();
-    let client = CuratorClient::connect("http://127.0.0.1:8080/events").await
+async fn curator_sse_client() -> Result<(), Box<dyn Error>> {
+    let server = Curator::start()?;
+    let mut client = SseClient::connect("http://127.0.0.1:8080/events")
+        .await
         .expect("Unable to connect");
     println!("STARTED");
 
-    delay_for(Duration::from_secs(5)).await;
-    //server.stop().await;
+    let mut i = 0;
+    while let Some(chunk) = client.next().await {
+        let chunk = chunk?;
+        i += 1;
+        if i >= 5 {
+            break;
+        }
+        println!("CHUNK: {:?}", chunk);
+    }
+
     drop(client);
+    server.stop().await;
+    Ok(())
 }
