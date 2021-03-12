9713db5af168602f92fdf981b04e84fa2b62d1c1
Denis Bazhenov <bazhenov@farpost.com>
2021-03-12 23:54:27 +1000

    Stream version of discovery loop

--- examples/docker-discovery.rs
+++ examples/docker-discovery.rs
@@ -4,19 +4,22 @@ use curator::{
     docker::{list_running_containers, run_toolchain_discovery},
     prelude::*,
 };
+use futures::stream::{Stream, StreamExt};
+use std::marker::Unpin;
 use termion::{clear, color, style};
-use tokio::time::{sleep, Duration};
+use tokio::time::{self, Duration};
+use tokio_stream::wrappers::IntervalStream;
 
 type TaskSet = Vec<(String, Vec<TaskDef>)>;
+const TOOLCHAIN: &str = "bazhenov.me/curator/toolchain-example:dev";
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let docker = Docker::connect_with_local_defaults()?;
 
-    let toolchain = "bazhenov.me/curator/toolchain-example:dev";
-
-    loop {
-        let task_set = build_task_set(&docker, toolchain).await?;
+    let mut discovery = start_discovery_polling_loop(&docker);
+    while let Some(task_set) = discovery.next().await {
+        let task_set = task_set?;
         println!("{}", clear::All);
         if !task_set.is_empty() {
             println!("Running containers:");
@@ -47,16 +50,26 @@ async fn main() -> Result<()> {
                 reset = style::Reset,
             )
         }
-        sleep(Duration::from_secs(1)).await;
     }
+    Ok(())
 }
 
-async fn build_task_set(docker: &Docker, toolchain: &str) -> Result<TaskSet> {
+async fn build_task_set(docker: &Docker) -> Result<TaskSet> {
     let mut result = vec![];
     for id in list_running_containers(&docker).await? {
-        let tasks = run_toolchain_discovery(&docker, &id, toolchain).await?;
+        let tasks = run_toolchain_discovery(&docker, &id, TOOLCHAIN).await?;
         let tuple = (id, tasks);
         result.push(tuple);
     }
     Ok(result)
+}
+
+fn start_discovery_polling_loop<'a>(
+    docker: &'a Docker,
+) -> impl Stream<Item = Result<TaskSet>> + Unpin + 'a {
+    let interval = time::interval(Duration::from_secs(1));
+    let stream = IntervalStream::new(interval);
+
+    let stream = stream.then(move |_| async move { build_task_set(docker).await });
+    Box::pin(stream)
 }
